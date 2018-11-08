f4cba5411b4af4569e2f3235801a881d71808503
Christopher McAdam <chrismcadam97@gmail.com>
2018-11-08 15:42:38 +1100

    updated views

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
+    url(r'^landing/', views.landing_page, name='landing_page'),
+    url(r'^features/', views.features_page, name='features_page'),
+    url(r'^contact/', views.contact_page, name='contact_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'home/home.html')
+
+def features_page(request):
+    return render(request, 'home/features.html')
+
+def contact_page(request):
+    return render(request, 'home/contact.html')
+
+def landing_page(request):
+    return render(request, 'home/landing.html')
